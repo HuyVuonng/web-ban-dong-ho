3474eaae4618a1a44e19cdf8766614668c56adf2
HuyVuong <vuonghuy12345@gmail.com>
2023-04-21 12:12:32 +0700

    xong phân trang

--- src/Layouts/Components/Header/Header.js
+++ src/Layouts/Components/Header/Header.js
@@ -104,10 +104,10 @@ function Header() {
                             <Link to="/">GIỚI THIỆU</Link>
                         </li>
                         <li className={cx('nav2-list-item')}>
-                            <Link to="/donghonam">ĐỒNG HỒ NAM</Link>
+                            <Link to="/donghonam?page=1">ĐỒNG HỒ NAM</Link>
                         </li>
                         <li className={cx('nav2-list-item')}>
-                            <Link to="/donghonu">ĐỒNG HỒ NỮ</Link>
+                            <Link to="/donghonu?page=1">ĐỒNG HỒ NỮ</Link>
                         </li>
                         <li className={cx('nav2-list-item')}>
                             <Link to="/">BLOGS</Link>
--- src/Pages/PhanLoai/PhanLoai.js
+++ src/Pages/PhanLoai/PhanLoai.js
@@ -2,29 +2,63 @@ import classNames from 'classnames/bind';
 import styles from './PhanLoai.module.scss';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 import httpRequest from '~/utils/httprequest';
 import ProductItem from '~/Components/ProductItem/ProductItem';
-
+import { useSearchParams } from 'react-router-dom';
 const cx = classNames.bind(styles);
 
 function PhanLoai() {
     const params = useParams();
-    const [products, setProducts] = useState([]);
+    const [ALLproducts, setALLProducts] = useState([]);
+    const [ProductInPagee, setProductInPagee] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams({});
     const gt = params.gt === 'donghonam' ? 'nam' : 'nu';
+    const soLuongItemTrenTrang = 2;
+    let currentPage = searchParams.get('page');
     const getProducts = async () => {
-        await httpRequest.get(`/products/gioitinh/${gt}`).then((response) => setProducts(response.data));
+        await httpRequest.get(`/products/gioitinh/${gt}`).then((response) => setALLProducts(response.data));
+        await httpRequest
+            .get(`/products/gioitinh/${gt}?page=${currentPage}`)
+            .then((response) => setProductInPagee(response.data));
     };
     useEffect(() => {
         params.gt === 'donghonam' ? (document.title = 'Đồng hồ nam') : (document.title = 'Đồng hồ nữ');
         getProducts();
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [params]);
+
+    const handlePageClick = async (event) => {
+        let page = event.selected + 1;
+        await httpRequest
+            .get(`/products/gioitinh/${gt}?page=${page}`)
+            .then((response) => setProductInPagee(response.data));
+        setSearchParams({ page: page });
+    };
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('content')}>
-                {products.map((product) => (
+                {ProductInPagee.map((product) => (
                     <ProductItem key={product._id} data={product} />
                 ))}
             </div>
+            <ReactPaginate
+                className={cx('phantrang')}
+                pageClassName={cx('phantrang-page')}
+                activeClassName={cx('phantrang-PageActive')}
+                pageLinkClassName={cx('phantrang-PageLink')}
+                marginPagesDisplayed={1}
+                forcePage={+currentPage - 1}
+                breakLabel="..."
+                nextLabel=">"
+                onPageChange={handlePageClick}
+                pageCount={ALLproducts.length / soLuongItemTrenTrang}
+                pageRangeDisplayed={2}
+                previousLabel="<"
+                renderOnZeroPageCount={null}
+            />
         </div>
     );
 }
